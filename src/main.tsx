d23d98bd07defa9bcc915322bde4f426957da646
Artur <arturbalthazar@gmail.com>
2025-11-20 00:28:40 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
+import Welcome from "./Welcome";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const path = window.location.pathname;
+
+createRoot(document.getElementById("root")!).render(
+  path.startsWith("/welcome") ? <Welcome /> : <App />
+);
 
 function setAppVh() {
   const h = window.visualViewport ? window.visualViewport.height : window.innerHeight;
